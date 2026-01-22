469b6848c3de216ab565233d5a3f2882d8fc7b18
VulcanWM <logmedha09@gmail.com>
2026-01-22 14:45:48 +0000

    added a button which detects when it's held down

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,51 @@
 import SwiftUI
 
+struct HoldButton: ButtonStyle {
+    let size: CGFloat
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: size, height: size)
+            .background(Color(red: 0.9, green: 0.4, blue: 0))
+            .foregroundStyle(.white)
+            .clipShape(Circle())
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+    }
+}
+
 struct ContentView: View {
+    @State private var isPressed: Bool = false
+    @State private var tapStatus: String = "hold to breathe"
+    
+    func hold() {
+        // action
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            let buttonSize = geo.size.width * 0.7
+
+            VStack {
+                Spacer()
+
+                Button(action: {
+                    self.tapStatus = "finished breathing"
+                    self.isPressed = false
+                }) {
+                    Text("HOLD")
+                        .font(.system(size: buttonSize * 0.25, weight: .bold))
+                }
+                .buttonStyle(HoldButton(size: buttonSize))
+                .simultaneousGesture(
+                    LongPressGesture(minimumDuration: 0.1).onEnded({ _ in
+                        self.tapStatus = "holding down"
+                        self.isPressed = true
+                    })
+                )
+                Text(tapStatus)
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
